229daddbfee3b260f733af67a90a8b710126981e
c20rtn <jack.corton@googlemail.com>
2020-04-12 22:05:43 +0100

    GAN outline working

--- gan.py
+++ gan.py
@@ -8,6 +8,8 @@ import os
 import random
 import time
 import cv2
+import math
+from IPython import display
 import matplotlib as mat
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
@@ -31,9 +33,11 @@ session = InteractiveSession(config=config)
 
 DATA_PATH  = '..\\Final Year Project\\Datasets\\cvcl.mit.edu\\**\\*.jpg'
 TEST_PATH  = 'test\\beach.jpg'
-EPOCHS = 100
+EPOCHS = 70
+BUFFER_SIZE = 5000
 BATCH_SIZE = 16
-EPOCH_STEPS = 16
+D_STEPS = 20
+G_STEPS = 15
 
 def get_images():
     print("\nGetting Images")
@@ -44,14 +48,14 @@ def get_images():
     X = np.array(X, dtype=np.uint8)
     print("\nData", X.shape)
     
-    labels = np.array([os.path.basename(os.path.dirname(image)) for image in files])
-    unique_labels = np.unique(labels).tolist()
-    print(unique_labels)
-    y = labels
-    y = np.array(list(map(lambda x: unique_labels.index(x), labels)))
-    print(np.unique(y))
+    # labels = np.array([os.path.basename(os.path.dirname(image)) for image in files])
+    # unique_labels = np.unique(labels).tolist()
+    # print(unique_labels)
+    # y = labels
+    # y = np.array(list(map(lambda x: unique_labels.index(x), labels)))
+    # print(np.unique(y))
     
-    return X, y
+    return X
 
 def dataset2lab(data):
     dataset = data.astype(np.float)
@@ -63,9 +67,13 @@ def get_test_image():
     testimages = glob.glob(TEST_PATH)
     testimage = img_to_array(load_img(testimages[0]))
     testimage = rgb2lab(1.0/255*testimage)[:,:,0]
-    testimage = testimage.reshape(1, 256, 256, 1)
+    testimage = np.expand_dims(testimage, axis=0)
+    testimage = np.expand_dims(testimage, axis=-1)
+    # testimage = testimage.reshape(256, 256, 1)
+    print("Test image -",testimage.shape)
+    return testimage
 
-def generator():
+def make_generator():
     print("\nCreating Generator")
     model = k.Sequential()
     model.add(InputLayer(input_shape=(256, 256, 1)))
@@ -88,7 +96,7 @@ def generator():
     
     return model
 
-def discriminator():
+def make_discriminator():
     print("\nCreating Discriminator")
     model = Sequential()
     model.add(Conv2D(32, (3, 3), padding='same', input_shape=(256,256,2), strides=2))
@@ -127,42 +135,137 @@ def train_test_LAB_values(X_train, X_test):
     X_train_AB = X_train[:,:,:,1:] 
     X_test_AB = X_test[:,:,:,1:] 
 
-    cur = np.zeros((256, 256, 3))
-    cur[:,:,0] = X_train_L[0,:,:,0]
-    cur[:,:,1:] = X_train_AB[0,:,:,:]
-    plt.imshow(lab2rgb(cur))
-    plt.show()
+    # cur = np.zeros((256, 256, 3))
+    # cur[:,:,0] = X_train_L[0,:,:,0]
+    # cur[:,:,1:] = X_train_AB[0,:,:,:]
+    # plt.imshow(lab2rgb(cur))
+    # plt.show()
     
     print("X_train_L shape : ", X_train_L.shape)
     print("X_test_L shape : ", X_test_L.shape)
     print("X_train_AB shape : ", X_train_AB.shape)
     print("X_test_AB shape : ", X_test_AB.shape)
+    print("SPLIT shape : ", X_test_AB[0:20].shape)
     return X_train_L, X_test_L, X_train_AB, X_test_AB
 
-if __name__ == '__main__':
-    
-    print("\nGAN STARTED")
-    
-    X, y = get_images()
-    # T = get_test_image()
-    # G = generator()
-    D = discriminator()
-    
-    # X_train, X_test = train_test_split(dataset2lab(X), test_size=0.2, random_state=42)
-    print("\nSet up train and test data")
-    # Set up train and test data
-    split = int(0.8*len(X))
-    Xtrain = X[:split]
-    Xtrain = 1.0/255*Xtrain
-    print("\nXtrain", Xtrain.shape)
-    X_train_L, X_test_L, X_train_AB, X_test_AB = train_test_LAB_values(X_train, X_test)
-    
-    print("\nCOMPILE STARTED")
-    D.compile(optimizer='adam',
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            metrics=['accuracy'])
-    print("\nTRAINING STARTED")
-    D.fit(X_train_L, X_train_AB, epochs=10)
-    test_loss, test_acc = D.evaluate(X_test_L, X_test_AB, verbose=2)
+def make_image(grayscale, generated):
+    cur = np.zeros((256, 256, 3))
+    cur[:,:,0] = generated[0][:,:,0]
+    cur[:,:,1:] = grayscale[0]
+    return lab2rgb(cur)
 
-    print('\nTest accuracy:', test_acc)
+X = get_images()
+X = dataset2lab(X)
+T = get_test_image()
+G = make_generator()
+D = make_discriminator()
+
+X_train, X_test = train_test_split(X, test_size=0.2, random_state=42)
+X_train_L, X_test_L, X_train_AB, X_test_AB = train_test_LAB_values(X_train, X_test)
+
+del X_train, X_test #Delete unused variables
+
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                discriminator_optimizer=discriminator_optimizer,
+                                generator=G,
+                                discriminator=D)
+
+
+@tf.function
+def train_step(images):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = G(T, training=True)
+
+        real_output = D(images, training=True)
+        fake_output = D(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, G.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, D.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, G.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, D.trainable_variables))
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+
+        image_batch = dataset[epoch*D_STEPS:(epoch*D_STEPS)+D_STEPS]
+
+        # for image_batch in dataset:
+        #     train_step(image_batch)
+            
+        train_step(image_batch)
+
+        # Produce images for the GIF as we go
+        # display.clear_output(wait=True)
+        # generate_and_save_images(generator,
+        #                         epoch + 1,
+        #                         seed)
+
+        # Save the model every 15 epochs
+        if (epoch + 1) % 15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print ('\nTime for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    
+    #Output image
+    print("\nOutput colorizations")
+    
+    output = G.predict(T)
+    output *= 128
+    
+    cur = np.zeros((256, 256, 3))
+    cur[:,:,0] = T[0,:,:,0]
+    cur[:,:,1:] = output[0]
+    plt.imshow(lab2rgb(cur)) 
+    plt.show()
+
+train(X_train_AB, EPOCHS)
+
+#Training loop for the GAN
+# for epoch in range(EPOCHS):
+#     print("EPOCH : ", epoch)
+#     start = time.time()
+#     #Train the detective
+#     for i in range(D_STEPS):
+#         #Train on real data batches
+#         print(epoch, " : Training on real data")
+#         D.fit(X_train_AB[(epoch*D_STEPS)+i],
+#                             np.ones(1)) #one label is generated
+        
+#         #Train on fake data batches
+#         print(epoch, " : Training on generated data")
+#         D.fit(G.predict(X_train_L[(epoch*D_STEPS)+i]), 
+#                             np.zeros(1)) #zero label is generated
+
+#     #Train the forger
+#     for epoch in range(G_STEPS):
+#         #Train the detective
+#         #Train on real data batches
+#         print(epoch, " : Training on real data")
+#         D.fit(batch_thingy, train_labels)
+    
+#     print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
